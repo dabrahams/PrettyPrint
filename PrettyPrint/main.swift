2e3765be16924f4c6412d7fbb79630a24d8159b5
Kyle Macomber <kmacomber@apple.com>
2018-02-02 14:40:32 -0800

    Initial Commit

---
+++ PrettyPrint/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PrettyPrint
+//
+//  Created by Kyle Macomber on 2/2/18.
+//  Copyright © 2018 Kyle Macomber. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
